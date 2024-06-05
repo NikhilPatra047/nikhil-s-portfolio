06942e072d18fff2428420867df9ce75e96cea9f
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-06-05 13:59:56 +0530

    add aria labels to links

--- src/app/(dashboard)/home/_sections/IntroSection.tsx
+++ src/app/(dashboard)/home/_sections/IntroSection.tsx
@@ -22,20 +22,20 @@ export default function IntroSection() {
         </div>
         <h3 className="w-fit text-left mt-8 text-[1.2rem] mx-auto">Hello there! 👋 I&apos;m a front-end web developer based in Bengaluru, India. I&apos;m passionate about learning and sharing my knowledge in the field of Computer Science.</h3>
         <div className="mt-4 max-sm:w-fit flex max-sm:flex-col sm:items-center">
-          <a href="mailto:patra.nikhil04@gmail.com" className="transition-all duration-300 ease-in-out bg-gradient-to-r from-purple-300 to-purple-500 dark:bg-gradient-to-r dark:from-purple-700 dark:to-purple-900 p-4 rounded-md inline-block">
+          <a aria-label="Click to send me an E-Mail" href="mailto:patra.nikhil04@gmail.com" className="transition-all duration-300 ease-in-out bg-gradient-to-r from-purple-300 to-purple-500 dark:bg-gradient-to-r dark:from-purple-700 dark:to-purple-900 p-4 rounded-md inline-block">
             Send me a message
           </a>
           <div className="sm:ml-6 max-sm:mt-4 max-sm:w-fit flex">
-            <a className="web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://www.linkedin.com/in/nikhil-k-patra/">
+            <a aria-label="Navigate to my LinkedIn profile" className="web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://www.linkedin.com/in/nikhil-k-patra/">
               <FaLinkedin size={30} />
             </a>
-            <a className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://github.com/NikhilPatra047">
+            <a aria-label="Navigate to my GitHub profile" className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://github.com/NikhilPatra047">
               <BsGithub size={30} />
             </a>
-            <a className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://hashnode.com/@Nikey47">
+            <a aria-label="Navigate to HashNode to check out my most recent blogs" className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://hashnode.com/@Nikey47">
               <FaHashnode size={30} />
             </a>
-            <a className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://drive.google.com/file/d/1idk08vXffMnlK7xOp7f_KqdbsRJWnCeK/view?usp=sharing">
+            <a aria-label="Click to check out my resume" className="ml-4 web_links hover:shadow-xl hover:shadow-purple-700" target="_blank" href="https://drive.google.com/file/d/1idk08vXffMnlK7xOp7f_KqdbsRJWnCeK/view?usp=sharing">
               <IoLink size={30} />
             </a>
           </div>
