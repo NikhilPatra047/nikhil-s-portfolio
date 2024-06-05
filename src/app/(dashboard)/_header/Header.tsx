3023c91b19076afcf4a7b0e52544a54953bf51d7
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-06-05 13:45:54 +0530

    wrap li around a tags

--- src/app/(dashboard)/_header/Header.tsx
+++ src/app/(dashboard)/_header/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   const [display, setDisplay] = useState<boolean>(true);
 
   const handleResize = () => {
-    setDisplay(window.innerWidth > 768); 
+    setDisplay(window.innerWidth > 768);
   };
 
   useEffect(() => {
@@ -24,7 +24,7 @@ export default function Header() {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []); 
+  }, []);
 
   return (
     <header className="p-4 max-w-[1500px] mx-auto dark:bg-black z-10 bg-white w-full border-b dark:border-white border-black sticky top-0 flex justify-between items-center">
@@ -38,21 +38,21 @@ export default function Header() {
         display &&
         (<nav className="w-[18em]">
           <ul className="text-[1.2rem] flex items-center justify-between">
-            <a href="/"><li className={`hover_links ${path.match(matchRegex) && 'border-b-[4px]'}`}>Home</li></a>
-            <a href="/about"><li className={`hover_links ${path.includes("/about") && 'border-b-[4px]'}`}>About</li></a>
-            <a href="/projects"><li className={`hover_links ${path.includes("/projects") && 'border-b-[4px]'}`}>Projects</li></a>
+            <li className={`hover_links ${path.match(matchRegex) && 'border-b-[4px]'}`}><a href="/">Home</a></li>
+            <li className={`hover_links ${path.includes("/about") && 'border-b-[4px]'}`}><a href="/about">About</a></li>
+            <li className={`hover_links ${path.includes("/projects") && 'border-b-[4px]'}`}><a href="/projects">Projects</a></li>
           </ul>
         </nav>)
       }
-      <button className={`${display === false? 'visible': 'hidden'}`} onClick={() => setIsOpen(!isOpen)} type="button">
-        {!isOpen? <GiHamburgerMenu aria-label="Navigation Menu" size={30} />: <ImCross size={30} />}
+      <button className={`${display === false ? 'visible' : 'hidden'}`} onClick={() => setIsOpen(!isOpen)} type="button">
+        {!isOpen ? <GiHamburgerMenu aria-label="Navigation Menu" size={30} /> : <ImCross size={30} />}
         {
           isOpen &&
           <nav className="w-full shadow-2xl border-2 border-black dark:border-white p-4 dark:bg-black bg-white absolute top-[100%] right-0">
             <ul className="text-[1.2rem] flex flex-col items-center justify-between">
-              <a href="/" aria-label="Navigate to the home page to check out my stats, work experience, get access to my blogs page, LinkedIn page, and resume."><li className="hover_links mb-6">Home</li></a>
-              <a href="/about" aria-label="Navigate to the about page to learn more about me and what I am passionate about."><li className="hover_links mb-6">About</li></a>
-              <a href="/projects" aria-label="Click on Projects to view all the projects that I have worked on over the years."><li className="hover_links mb-6">Projects</li></a>
+              <li className="hover_links mb-6"><a href="/" aria-label="Navigate to the home page to check out my stats, work experience, get access to my blogs page, LinkedIn page, and resume.">Home</a></li>
+              <li className="hover_links mb-6"><a href="/about" aria-label="Navigate to the about page to learn more about me and what I am passionate about.">About</a></li>
+              <li className="hover_links mb-6"><a href="/projects" aria-label="Click on Projects to view all the projects that I have worked on over the years.">Projects</a></li>
             </ul>
           </nav>
         }
